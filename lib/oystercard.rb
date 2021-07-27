6789e048b4585d693ebb3d65a5dfcdbe8dbef346
Dylan-P-H <Dylan@eequ.org>
2021-07-27 21:09:47 +0100

    completed question 11 but not happy with code quality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,32 +1,47 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :card_state, :in_journey, :entry_station
+
   BALANCE_LIMIT = 90
   MINIMUM_FAIR = 1
+
   def initialize
     @balance = 0
     @card_state = "DEACTIVATED"
+    @in_journey = false
+    @entry_station = nil
   end
+
   def top_up(amount)
     fail "Balance limit is #{BALANCE_LIMIT}" if @balance == 90
     @balance += amount
   end
-  def touch_in
+
+  def touch_in(station)
     fail "Insufficient funds for minimum fair" if @balance < MINIMUM_FAIR
     @card_state = "ACTIVATED"
+    @in_journey = true
+    @entry_station = station
   end
+
   def touch_out
-    @balance -= 1
+    deduct(MINIMUM_FAIR)
+    @entry_station = nil
     @card_state = "DEACTIVED"
   end
+
   def in_journey?
     if @card_state == "ACTIVATED"
       return true
     else
       return false
     end
+    !!entry_station
   end
+
   private
+
   def deduct(amount)
     @balance -= amount
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,20 +1,26 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  let(:station) { double :station }
+
   it "Has a balance of zero" do
     expect(subject.balance).to eq(0)
   end
+
   describe "#top_up" do
     it { is_expected.to respond_to(:top_up).with(1).argument }
     it "Should top up balance" do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
+
     it 'raises an error if the maximum balance is exceeded' do
       balance_limit = Oystercard::BALANCE_LIMIT
       subject.top_up(balance_limit)
       expect{ subject.top_up 1 }.to raise_error "Balance limit is #{balance_limit}"
     end
   end
+
   describe "#deduct" do
     it "Should deduct amount from balance" do
       test = Oystercard.new
@@ -23,21 +29,32 @@ describe Oystercard do
       expect( test.balance ).to eq(0)
     end
   end
+
   it "If user has touched in, in_journey? method should be true" do
     subject.top_up(10)
-    subject.touch_in
+    subject.touch_in(:station)
     expect( subject.in_journey? ).to eq(true)
   end
+
   it "Should change card_state if user invokes touch_out" do
     subject.touch_out
     expect( subject.in_journey? ).to eq(false)
   end
+
   it "Should fail if balance is less than minumum fair when touching in" do
     subject.top_up(0)
-    expect{ subject.touch_in }.to raise_error "Insufficient funds for minimum fair"
+    expect{ subject.touch_in(:station) }.to raise_error "Insufficient funds for minimum fair"
   end
+
   it "Should deduct from my balance when I make a trip" do
     subject.top_up(10)
     expect { subject.touch_out }.to change{ subject.balance }.by(-1)
   end
+
+  it "stores the entry station" do
+    subject.top_up(10)
+    subject.touch_in(:station)
+    expect(subject.entry_station).to eq(:station)
+  end
+
 end
